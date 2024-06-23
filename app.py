bf02dff452abda3c9d9c06ff1678507c34cc2551
Saurabhf <Saurabh.funde@hotmail.com>
2024-06-23 23:25:20 +0530

    text summaraization app

--- app.py
+++ app.py
@@ -1,1 +1,49 @@
 from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain_huggingface.llms import HuggingFacePipeline
+from langserve import add_routes
+import uvicorn
+from langchain_core.output_parsers import StrOutputParser
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="google-t5/t5-small",
+    task="summarization",
+    pipeline_kwargs={"max_new_tokens": 100},
+)
+
+# 1. Create prompt template
+system_template = "Summaraize the given text: {text}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = llm
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="Text Summaraization App",
+  version="1.0",
+  description="A simple text summary app",
+)
+
+# 5. Adding chain route
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
